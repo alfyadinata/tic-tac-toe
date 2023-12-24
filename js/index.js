7b4b67bf1a247d2006ce5e20171f49c62d524fbe
alfyadinata <alfymuhammad7@gmail.com>
2023-12-24 22:36:59 +0700

    fix check winner

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     const app = document.getElementById('app');
     let currentPlayer = 'X';
-    const scale = 3;
-    let cells = Array.from({ length: scale * scale }, () => "");
+    let scale = 3;
+    let cells = 9;
+
+    const handleStart = () => {
+        if (scale < 3) {
+            return alert("Minimum scale is 3");
+        }
+        document.getElementById('form').style.display = 'none';
+        document.getElementById('app').style.setProperty('--scale', scale);
+        document.getElementById('app').style.display = 'grid';
+
+        scale = document.getElementById('scale').value;
+        cells = Array.from({ length: scale * scale }, () => "");
+        console.log("uhuhu", cells)
+    }
+
+    document.getElementById('start').addEventListener('click', handleStart);
+
 
     const draw = () => {
         app.innerHTML = '';
@@ -38,27 +54,53 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const isWinnerByDiagonal = () => {
-        const firstDiagonal = [];
-        const secondDiagonal = [];
-
-        for (let index = 0; index < cells.length; index++) {
-            firstDiagonal.push(cells[index * scale + 1]);
-            secondDiagonal.push(cells[index * scale + (scale - 1 - index)]);
+        const leftToRight = [];
+        const rightToLeft = [];
+        for (let index = 0; index < scale; index++) {
+            if (index > 0) {
+                console.log('scalee', scale)
+                console.log('apa', index * scale + 1);
+            }
+            leftToRight.push(index === 0 ? cells[0] : cells[index * scale + 1]);
+            rightToLeft.push(cells[index * scale + (scale - 1 - index)]);
         }
 
-        const isWinByFirstDiagonal = firstDiagonal.every((cell) => cell === currentPlayer);
-        const isWinBySecondDiagonal = secondDiagonal.every((cell) => cell === currentPlayer);
-        const result = isWinByFirstDiagonal || isWinBySecondDiagonal || false;
+        const isWinByleftToRight = leftToRight.every((cell) => cell === currentPlayer);
+        const isWinByrightToLeft = rightToLeft.every((cell) => cell === currentPlayer);
+
+        console.log('cell', cells)
+        console.log('leftToRight',leftToRight)
+        console.log('rightToLeft', rightToLeft)
+
+        const result = isWinByleftToRight || isWinByrightToLeft || false;
  
         return result;
     }
 
+    const isWinnerByVertical = () => {
+        for (let index = 0; index < scale; index++) {
+            const verticalRows = [];
+            const cellIndex = index;
+
+            for (let indexChild = 0; indexChild < scale; indexChild++) {
+                verticalRows.push(cells[cellIndex + indexChild * scale]);
+            }
+
+            const result = verticalRows.every((cell) => cell === currentPlayer);
+
+            if (result) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     const isHasAWinner = () => {
-        console.log("isWinnerByRow()", isWinnerByRow());
-        console.log("isWinnerByDiagonal", isWinnerByDiagonal());
-        console.log("cells", cells)
-        return isWinnerByRow() || isWinnerByDiagonal() || false;
+        console.log('isWinnerByRow', isWinnerByRow())
+        console.log('isWinnerByVertical', isWinnerByVertical())
+        console.log('isWinnerByDiagonal', isWinnerByDiagonal())
+        return isWinnerByRow() || isWinnerByDiagonal() || isWinnerByVertical() || false;
     };
 
     const isDraw = () => cells.every((cell) => cell !== '');
@@ -72,14 +114,14 @@ document.addEventListener("DOMContentLoaded", function () {
             draw();
 
             if (isHasAWinner()) {
-                console.log("done", cells);
-            }
-
-            if (isDraw()) {
+                alert(`${currentPlayer} is win the game`);
+                window.location.reload();
+            } else if (isDraw()) {
                 alert('draw');
+            } else {
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             }
 
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         }
     }
 
